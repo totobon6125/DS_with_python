59206f788a8ca24abdb456a515c4604a7bf8623c
totobon6125 <totobon6125@gmail.com>
2024-02-23 18:58:27 +0900

    day06

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-def recursion_func():
-   print('난 재귀')
-   recursion_func()
-recursion_func()
+my_lst = [0 for _ in range(10)]
+
+my_lst2 = [[0]*3 for _ in range(10)]
+
+print(my_lst)
+print(my_lst2)
